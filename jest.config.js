f95b68b0bbbc180c31fa0ee30de3b0ce14155200
RobertRoz3 <osceoalrob@gmail.com>
2023-09-09 00:19:42 -0400

    jest attempt

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    roots: [
+        "<rootDir>/tests/"
+    ],
+    transform: {
+        "^.+\\.(ts|tsx)$": "ts-jest"
+    },
+    };
