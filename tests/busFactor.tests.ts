5f3b2d773ba26562b2fd13996e78c083a2c0ffe1
RobertRoz3 <osceoalrob@gmail.com>
2023-09-10 18:08:38 -0400

    delete

--- tests/busFactor.tests.ts
+++
@@ -1,9 +0,0 @@
-import { getBusFactor } from '../src/busFactor';
-
-test('calculates the bus factor correctly', async () => {
-    const repoUrl = 'https://github.com/netdata/netdata';
-    const result = await getBusFactor(repoUrl);
-    
-    expect(result.busFactor).toBeGreaterThanOrEqual(0);
-    expect(result.busFactor).toBeLessThanOrEqual(1);
-});
