f9d70f1d1e86a281f45294e76d5f3eaa82bdae9e
RobertRoz3 <osceoalrob@gmail.com>
2023-09-10 18:07:40 -0400

    basic testing

---
+++ tests/correctness.test.ts
@@ -0,0 +1,14 @@
+import { fetchCorrectnessData } from '../src/correctness'; 
+import dotenv from 'dotenv';
+
+dotenv.config({ path: 'C:\\Users\\osceo\\Desktop\\Stuff\\ECE461\\ECE-461-Group-12-FA2023-project-part1\\src\\.env' }); //change this to your direct path, idk why the relative path refuses to work
+
+test('calculates the correctness score correctly', async () => {
+    const spy = jest.spyOn(console, 'error').mockImplementation(() => {});
+
+    const repoUrl = 'https://github.com/wolever/parameterized'; 
+    const result = await fetchCorrectnessData(repoUrl);
+    
+    expect(result.correctnessScore).toBeGreaterThanOrEqual(0);
+    expect(result.correctnessScore).toBeLessThanOrEqual(1);
+});
