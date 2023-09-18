37e0793c1dde7988df96ad8718da159cf298f873
Rachit Bisht <99228844+rvchit@users.noreply.github.com>
2023-09-18 15:13:44 -0400

    Update rampUp.ts

--- src/rampUp.ts
+++ src/rampUp.ts
@@ -1,81 +1,55 @@
+import git from 'isomorphic-git';
 import fs from 'fs';
-import { GitConfig, init, fetch, readObject } from 'isomorphic-git';
-import http from 'isomorphic-git/http/node';
+import os from 'os';
+import path from 'path';
+import dotenv from 'dotenv';
 import createModuleLogger from './logger';
+import http from 'isomorphic-git/http/node';
 
+dotenv.config();
 const logger = createModuleLogger('Ramp Up');
-
-// Initialize the git repository
-async function initializeGitRepository() {
-  const config: GitConfig = {
-    fs,
-    http,
-  };
-  await init(config);
-}
-
-// download the README content
-async function downloadReadme(owner: string, repo: string): Promise<string | null> {
-  const config: GitConfig = {
-    fs,
-    http,
-    onAuth: () => ({
-      // Use OAuth token for authentication (recommended)
-      token: process.env.GITHUB_TOKEN,
-    }),
-  };
-
-  try {
-    await fetch({
-      ...config,
-      url: `https://github.com/${owner}/${repo}.git`,
-    });
-
-    const { object: { blob: content } } = await readObject(config, {
-      dir: `./${repo}`, // needs to be changed to the appropriate directory
-      oid: 'HEAD:README.md', // have to use the correct path to the README file, I can change this
-    });
-
-    return Buffer.from(content).toString('utf-8');
-  } catch (error) {
-    logger.error(`Failed to download README for ${repo}. Error: ${error.message}`);
-    return null;
-  }
-}
 
 export async function rampUp(url: string): Promise<number> {
   const urlParts = url.split('/');
   const repo = urlParts.pop();
   const owner = urlParts.pop();
+  const tempDir = path.join(os.tmpdir(), `repo-${Date.now()}`);
 
-  await initializeGitRepository();
-  const readme = await downloadReadme(owner, repo);
+  try {
+    await fs.promises.mkdir(tempDir);
+    await git.clone({ fs, http, dir: tempDir, url: `https://github.com/${owner}/${repo}.git`, singleBranch: true, depth: 1 });
 
-  if (!readme) {
-    logger.info(`Couldn't get readme for rampUp. Score of 0`);
-    return 0;
+    const readmePath = path.join(tempDir, 'README.md');
+    if (!fs.existsSync(readmePath)) {
+      logger.info("Couldn't get readme for rampUp. Score of 0");
+      return 0;
+    }
+
+    const readme = await fs.promises.readFile(readmePath, 'utf-8');
+    const readmeLines = readme.split('\n').length;
+
+    // Readme length score calculation
+    let score = 0;
+    if (readmeLines > 0) {
+      score += 0.2 * Math.min(readmeLines / 200, 1);
+    }
+
+    // Keywords score calculation using regex pattern
+    const keywordPattern = /Installation|Features|Quick Start|Wiki|Guide|Examples/gi;
+    const matches = readme.match(keywordPattern);
+
+    if (matches) {
+      // use a Set to eliminate duplicate matches, then get the unique count
+      const uniqueMatches = new Set(matches.map((match) => match.toLowerCase()));
+      score += 0.16 * uniqueMatches.size;
+    }
+
+    // if score is greater than 1, return 1
+    return Math.min(score, 1);
+  } catch (error) {
+    logger.error('Error during rampUp', error);
+    throw error;
+  } finally {
+    await fs.promises.rm(tempDir, { recursive: true, force: true });
   }
-
-  const readmeLines = readme.split('\n').length;
-
-  // Readme length score calculation
-  let score = 0;
-  if (readmeLines > 0) {
-    score += 0.2 * Math.min(readmeLines / 200, 1);
-  }
-
-  // Keywords score calculation using regex pattern
-  const keywordPattern = /Installation|Features|Quick Start|Wiki|Guide|Examples/gi;
-  const matches = readme.match(keywordPattern);
-
-  if (matches) {
-    // Use a Set to eliminate duplicate matches, then get the unique count
-    const uniqueMatches = new Set(matches.map((match) => match.toLowerCase()));
-    score += 0.16 * uniqueMatches.size;
-  }
-
-  if (score > 1) {
-    return 1;
-  }
-  return score;
 }
