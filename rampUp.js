6678f65d8700aced1f222f23413259cd6dad5aef
Rachit Bisht <99228844+rvchit@users.noreply.github.com>
2023-09-18 16:43:09 -0400

    Update rampUp.js

--- rampUp.js
+++ rampUp.js
@@ -1,48 +1,62 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.rampUp = void 0;
-const node_fetch_1 = __importDefault(require("node-fetch"));
-const dotenv_1 = __importDefault(require("dotenv"));
-const logger_1 = __importDefault(require("./logger"));
-const logger = (0, logger_1.default)('Ramp Up');
-dotenv_1.default.config();
-async function rampUp(url) {
-    const urlParts = url.split('/');
-    const repo = urlParts.pop();
-    const owner = urlParts.pop();
-    const apiURL = `https://api.github.com/repos/${owner}/${repo}/readme`;
-    const response = await (0, node_fetch_1.default)(apiURL, {
-        headers: {
-            Authorization: `token ${process.env.GITHUB_TOKEN}`,
-        },
-    });
-    if (!response.ok) {
-        logger.info("Couldn't get readme for rampuUp. Score of 0");
-        return 0;
-    }
-    const { content } = await response.json();
-    const readme = Buffer.from(content, 'base64').toString('utf-8');
-    const readmeLines = readme.split('\n').length;
-    // Readme length score calculation
-    let score = 0;
-    if (readmeLines > 0) {
-        score += 0.2 * Math.min(readmeLines / 200, 1);
-    }
-    // Keywords score calculation using cregex pattern
-    const keywordPattern = /Installation|Features|Quick Start|Wiki|Guide|Examples/gi;
-    const matches = readme.match(keywordPattern);
-    if (matches) {
-        // use a Set to eliminate duplicate matches, then get the unique count
-        const uniqueMatches = new Set(matches.map((match) => match.toLowerCase()));
-        score += 0.16 * uniqueMatches.size;
-    }
-    //if score is greater than 1, return 1
-    if (score > 1) {
-        return 1;
-    }
-    return score;
+import git from 'isomorphic-git';
+import fs from 'fs';
+import os from 'os';
+import path from 'path';
+import dotenv from 'dotenv';
+import createModuleLogger from './logger';
+import http from 'isomorphic-git/http/node';
+
+dotenv.config();
+const logger = createModuleLogger('Ramp Up');
+
+export async function rampUp(url: string): Promise<number> {
+	const urlParts = url.split('/');
+	const repo = urlParts.pop();
+	const owner = urlParts.pop();
+	const tempDir = path.join(os.tmpdir(), `repo-${Date.now()}`);
+
+	try {
+		await fs.promises.mkdir(tempDir);
+		await git.clone({
+			fs,
+			http,
+			dir: tempDir,
+			url: `https://github.com/${owner}/${repo}.git`,
+			singleBranch: true,
+			depth: 1,
+		});
+
+		const readmePath = path.join(tempDir, 'README.md');
+		if (!fs.existsSync(readmePath)) {
+			logger.info("Couldn't get readme for rampUp. Score of 0");
+			return 0;
+		}
+
+		const readme = await fs.promises.readFile(readmePath, 'utf-8');
+		const readmeLines = readme.split('\n').length;
+
+		// Readme length score calculation
+		let score = 0;
+		if (readmeLines > 0) {
+			score += 0.2 * Math.min(readmeLines / 200, 1);
+		}
+
+		// Keywords score calculation using regex pattern
+		const keywordPattern = /Installation|Features|Quick Start|Wiki|Guide|Examples/gi;
+		const matches = readme.match(keywordPattern);
+
+		if (matches) {
+			// use a Set to eliminate duplicate matches, then get the unique count
+			const uniqueMatches = new Set(matches.map((match) => match.toLowerCase()));
+			score += 0.16 * uniqueMatches.size;
+		}
+
+		// if score is greater than 1, return 1
+		return Math.min(score, 1);
+	} catch (error) {
+		logger.error('Error during rampUp', error);
+		throw error;
+	} finally {
+		await fs.promises.rm(tempDir, { recursive: true, force: true });
+	}
 }
-exports.rampUp = rampUp;
