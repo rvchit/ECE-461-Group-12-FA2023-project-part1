dd15fc0809cf1a71e6d205397b9da47c391aea69
Rachit Bisht <99228844+rvchit@users.noreply.github.com>
2023-09-19 13:11:02 -0400

    Update rampUp.ts

--- src/rampUp.ts
+++ src/rampUp.ts
@@ -11,52 +11,52 @@ dotenv.config();
 const logger = createModuleLogger('Ramp Up');
 
 export async function rampUp(url: string): Promise<number> {
-  let score = 0;
+	let score = 0;
 
-  try {
-    // Create a temporary directory to clone the repository
-    const { path: localPath, cleanup } = await dir();
+	try {
+		// Create a temporary directory to clone the repository
+		const { path: localPath, cleanup } = await dir();
 
-    // Clone the repository using isomorphic-git
-    await git.clone({ fs, http, dir: localPath, url });
+		// Clone the repository using isomorphic-git
+		await git.clone({ fs, http, dir: localPath, url });
 
-    // Try reading the README.md file from the local clone
-    let readme;
-    try {
-      readme = fs.readFileSync(path.join(localPath, 'README.md'), 'utf-8');
-    } catch {
-      logger.info("Couldn't find README.md locally; Score of 0");
-      await cleanup();
-      return 0;
-    }
+		// Try reading the README.md file from the local clone
+		let readme;
+		try {
+			readme = fs.readFileSync(path.join(localPath, 'README.md'), 'utf-8');
+		} catch {
+			logger.info("Couldn't find README.md locally; Score of 0");
+			await cleanup();
+			return 0;
+		}
 
-    // Calculate ramp up score based on the README content
-    const readmeLines = readme.split('\n').length;
+		// Calculate ramp up score based on the README content
+		const readmeLines = readme.split('\n').length;
 
-    if (readmeLines > 0) {
-      score += 0.2 * Math.min(readmeLines / 200, 1);
-    }
+		if (readmeLines > 0) {
+			score += 0.2 * Math.min(readmeLines / 200, 1);
+		}
 
-    // Keywords score calculation using regex pattern
-    const keywordPattern = /Installation|Features|Quick Start|Wiki|Guide|Examples/gi;
-    const matches = readme.match(keywordPattern);
+		// Keywords score calculation using regex pattern
+		const keywordPattern = /Installation|Features|Quick Start|Wiki|Guide|Examples/gi;
+		const matches = readme.match(keywordPattern);
 
-    if (matches) {
-      const uniqueMatches = new Set(matches.map((match) => match.toLowerCase()));
-      score += 0.16 * uniqueMatches.size;
-    }
+		if (matches) {
+			const uniqueMatches = new Set(matches.map((match) => match.toLowerCase()));
+			score += 0.16 * uniqueMatches.size;
+		}
 
-    // Cleanup the cloned repository
-    await cleanup();
-    
-    // If score is greater than 1, return 1
-    if (score > 1) {
-      return 1;
-    }
+		// Cleanup the cloned repository
+		await cleanup();
 
-    return score;
-  } catch (error) {
-    logger.error('Error in rampUp function', error);
-    return score;
-  }
+		// If score is greater than 1, return 1
+		if (score > 1) {
+			return 1;
+		}
+
+		return score;
+	} catch (error) {
+		logger.error('Error in rampUp function', error);
+		return score;
+	}
 }
