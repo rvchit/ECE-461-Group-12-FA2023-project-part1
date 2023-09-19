7922234a5dc07c528482e8462af673516bddbb28
Rachit Bisht <99228844+rvchit@users.noreply.github.com>
2023-09-19 13:03:18 -0400

    Update rampUp.ts

--- src/rampUp.ts
+++ src/rampUp.ts
@@ -1,49 +1,62 @@
-import fetch from 'node-fetch';
 import dotenv from 'dotenv';
 import createModuleLogger from './logger';
+import git from 'isomorphic-git';
+import http from 'isomorphic-git/http/node';
+import fs from 'fs';
+import path from 'path';
+import { dir } from 'tmp-promise';
 
-const logger = createModuleLogger('Ramp Up');
 dotenv.config();
 
+const logger = createModuleLogger('Ramp Up');
+
 export async function rampUp(url: string): Promise<number> {
-	const urlParts = url.split('/');
-	const repo = urlParts.pop();
-	const owner = urlParts.pop();
-	const apiURL = `https://api.github.com/repos/${owner}/${repo}/readme`;
+  let score = 0;
 
-	const response = await fetch(apiURL, {
-		headers: {
-			Authorization: `token ${process.env.GITHUB_TOKEN}`,
-		},
-	});
+  try {
+    // Create a temporary directory to clone the repository
+    const { path: localPath, cleanup } = await dir();
 
-	if (!response.ok) {
-		logger.info("Couldn't get readme for rampuUp. Score of 0");
-		return 0;
-	}
+    // Clone the repository using isomorphic-git
+    await git.clone({ fs, http, dir: localPath, url });
 
-	const { content } = await response.json();
-	const readme = Buffer.from(content, 'base64').toString('utf-8');
-	const readmeLines = readme.split('\n').length;
+    // Try reading the README.md file from the local clone
+    let readme;
+    try {
+      readme = fs.readFileSync(path.join(localPath, 'README.md'), 'utf-8');
+    } catch {
+      logger.info("Couldn't find README.md locally; Score of 0");
+      await cleanup();
+      return 0;
+    }
 
-	// Readme length score calculation
-	let score = 0;
-	if (readmeLines > 0) {
-		score += 0.2 * Math.min(readmeLines / 200, 1);
-	}
+    // Calculate ramp up score based on the README content
+    const readmeLines = readme.split('\n').length;
 
-	// Keywords score calculation using cregex pattern
-	const keywordPattern = /Installation|Features|Quick Start|Wiki|Guide|Examples/gi;
-	const matches = readme.match(keywordPattern);
+    if (readmeLines > 0) {
+      score += 0.2 * Math.min(readmeLines / 200, 1);
+    }
 
-	if (matches) {
-		// use a Set to eliminate duplicate matches, then get the unique count
-		const uniqueMatches = new Set(matches.map((match) => match.toLowerCase()));
-		score += 0.16 * uniqueMatches.size;
-	}
-	//if score is greater than 1, return 1
-	if (score > 1) {
-		return 1;
-	}
-	return score;
+    // Keywords score calculation using regex pattern
+    const keywordPattern = /Installation|Features|Quick Start|Wiki|Guide|Examples/gi;
+    const matches = readme.match(keywordPattern);
+
+    if (matches) {
+      const uniqueMatches = new Set(matches.map((match) => match.toLowerCase()));
+      score += 0.16 * uniqueMatches.size;
+    }
+
+    // Cleanup the cloned repository
+    await cleanup();
+    
+    // If score is greater than 1, return 1
+    if (score > 1) {
+      return 1;
+    }
+
+    return score;
+  } catch (error) {
+    logger.error('Error in rampUp function', error);
+    return score;
+  }
 }
