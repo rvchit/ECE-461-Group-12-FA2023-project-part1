69088260bc78989d4421e2c61b1390033ded92d3
Rachit Bisht <99228844+rvchit@users.noreply.github.com>
2023-09-19 18:44:39 -0400

    Update rampup.test.ts

--- test/rampup.test.ts
+++ test/rampup.test.ts
@@ -7,14 +7,14 @@ const gitURL1 = 'https://github.com/tj/commander.js';
 test('it should return a score of 0 for a repository with no README', async () => {
 	const score = await rampUp(gitURL0);
 	expect(score).toBe(0);
-});
+}, 15000);
 
 test('it should return a score of 0.744 for a repository with a README and some keywords', async () => {
 	const score = await rampUp(gitURLmiddle);
 	expect(score).toBe(0.744);
-});
+}, 15000);
 
 test('it should return a score of 1 for a repository with perfect README', async () => {
 	const score = await rampUp(gitURL1);
 	expect(score).toBe(1);
-});
+}, 15000);
